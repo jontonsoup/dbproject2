a2a42474e4d9a524efe155a3d1591f0ae9beb935
Jonathan Friedman <friedmanj98@gmail.com>
2012-11-10 14:24:56 -0600

    Bug Fix: I'm a G and G isnt.

--- header.pl
+++ header.pl
@@ -1,66 +1,11 @@
 #!/usr/bin/perl -w
 
-#
-#
-
-
-#
-# Debugging
-#
-# database input and output is paired into the two arrays noted
-#
-my $debug=1; # default - will be overriden by a form parameter or cookie
-my @sqlinput=();
-my @sqloutput=();
 use strict;
-use CGI qw(:standard);
+use CGI;
+use CGI::Carp qw(fatalsToBrowser);
 use DBI;
 use Time::ParseDate;
-
-# my $dbuser="jmf716";
-# my $dbpasswd="RR62rwno";
-
-
-# my $cookiename="RWBSession";
-# #
-# # And another cookie to preserve the debug state
-# #
-# my $debugcookiename="RWBDebug";
-
-# #
-# # Get the session input and debug cookies, if any
-# #
-# my $inputcookiecontent = cookie($cookiename);
-# my $inputdebugcookiecontent = cookie($debugcookiename);
-
-# #
-# # Will be filled in as we process the cookies and paramters
-# #
-# my $outputcookiecontent = undef;
-# my $deletecookie=0;
-# # get the user action and whether he just wants the form or wants us to
-# # run the form
-# #
-# my $action;
-# my $run;
-
-
-# $outputdebugcookiecontent=$debug;
-
-
-
-# my @outputcookies;
-
-
-# if (defined($outputcookiecontent)) {
-# 	my $cookie=cookie(-name=>$cookiename,
-# 		-value=>$outputcookiecontent,
-# 		-expires=>($deletecookie ? '-1h' : '+1h'));
-# 	push @outputcookies, $cookie;
-# }
-
-
-print header(-expires=>'now');
+print "Content-type: text/html\n\n";
 
 print "<html>";
 print "<head>";

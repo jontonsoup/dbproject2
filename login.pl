5f41d8651ab58683da50ba76fff1c4207eb531e9
Jonathan Friedman <friedmanj98@gmail.com>
2012-11-10 16:21:28 -0600

    Feature: login isnt really functional, but it inserts some stuff

--- login.pl
+++ login.pl
@@ -1,13 +1,22 @@
 #!/usr/bin/perl -w
-
 require "header.pl";
+use Data::Dumper;
+
+if ($ENV{'REQUEST_METHOD'} eq "POST") {
+	my $user = param("user");
+	my $pass = param("password");
+	my @rows;
+	eval { @rows = ExecSQL($dbuser,$dbpasswd,"insert into stockuser (email, password) values ('$user', '$pass')");};
+}
+
+else {
+
+	print start_form(-name=>'Login', -type=>"post"),
+	h2('Login!'), "<fieldset>",
+	"Name: ",textfield(-name=>'user'),"<br><br>",
+	"Password: ",password_field(-name=>'password'), "<br><br>";
+	print "<input type=\"submit\" class=\"btn btn-primary\">","</fieldset>";
 
 
-print start_form(-name=>'Login'),
-h2('Login!'), "<fieldset>",
-"Name: ",textfield(-name=>'user'),"<br><br>",
-"Password: ",password_field(-name=>'password'), "<br><br>";
-print "<input type=\"submit\" class=\"btn btn-primary\">","</fieldset>";
-
+}
 require "footer.pl";
-

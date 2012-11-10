b37d8fbe991b48f3600a7e811a9fb69784d97dcf
Jonathan Friedman <friedmanj98@gmail.com>
2012-11-10 00:02:56 -0600

    Feature-BROKEN: added header and login files

---
+++ header.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl -w
+use CGI::Carp qw(fatalsToBrowser);
+
+print "Content-type: text/html\n\n";
+print "<html>";
+print "<head>";
+print "<link href=\"//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.1/css/bootstrap-combined.min.css\" rel=\"stylesheet\">";
+
+print "</head>";
---
+++ login.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl -w
+
+print "<html>"
+print "<head>"
+print "<link href=\"//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.1/css/bootstrap-combined.min.css\" rel=\"stylesheet\">"
+
+print "</head>"

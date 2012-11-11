eff26e08fa871d6b0dbf3bbe054200567fc0432b
Jonathan Friedman <friedmanj98@gmail.com>
2012-11-10 23:24:50 -0600

    Feature: cookies!

--- header.pl
+++ header.pl
@@ -25,7 +25,7 @@ $dbpasswd="z1OlkliU6";
 # AND CONSIDER SUPPORTING ONLY HTTPS
 
 my $cookiename = "PSession";
-my $debugcookiename = "PDebugSession"
+my $debugcookiename = "PDebugSession";
 
 # grab session input cookie, if any; also, debug cookie, if any
 my $inputcookiecontent = cookie($cookiename);
@@ -36,47 +36,47 @@ my $inputdebugcookiecontent = cookie($debugcookiename);
 
 
 sub MakeTable {
-  my ($id,$type,$headerlistref,@list)=@_;
-  my $out;
+	my ($id,$type,$headerlistref,@list)=@_;
+	my $out;
 #
 # Check to see if there is anything to output
 #
-  if ((defined $headerlistref) || ($#list>=0)) {
+if ((defined $headerlistref) || ($#list>=0)) {
 # if there is, begin a table
 #
-    $out="<table class=\"table table-striped\" id=\"$id\" border>";
+$out="<table class=\"table table-striped\" id=\"$id\" border>";
 #
 # if there is a header list, then output it in bold
 #
-    if (defined $headerlistref) {
-      $out.="<tr>".join("",(map {"<td><b>$_</b></td>"} @{$headerlistref}))."</tr>";
-    }
+if (defined $headerlistref) {
+	$out.="<tr>".join("",(map {"<td><b>$_</b></td>"} @{$headerlistref}))."</tr>";
+}
 #
 # If it's a single row, just output it in an obvious way
 #
-    if ($type eq "ROW") {
+if ($type eq "ROW") {
       #
       # map {code} @list means "apply this code to every member of the list
       # and return the modified list.  $_ is the current list member
       #
       $out.="<tr>".(map {defined($_) ? "<td>$_</td>" : "<td>(null)</td>" } @list)."</tr>";
-    } elsif ($type eq "COL") {
+      } elsif ($type eq "COL") {
       #
       # ditto for a single column
       #
       $out.=join("",map {defined($_) ? "<tr><td>$_</td></tr>" : "<tr><td>(null)</td></tr>"} @list);
-    } else {
+      } else {
       #
       # For a 2D table, it's a bit more complicated...
       #
       $out.= join("",map {"<tr>$_</tr>"} (map {join("",map {defined($_) ? "<td>$_</td>" : "<td>(null)</td>"} @{$_})} @list));
-    }
-    $out.="</table>";
+  }
+  $out.="</table>";
   } else {
     # if no header row or list, then just say none.
     $out.="(none)";
-  }
-  return $out;
+}
+return $out;
 }
 
 #
@@ -90,40 +90,35 @@ sub MakeTable {
 # ExecSQL executes "die" on failure.
 #
 sub ExecSQL {
-  my ($user, $passwd, $querystring, $type, @fill) =@_;
-  if ($debug) {
+	my ($user, $passwd, $querystring, $type, @fill) =@_;
+	if ($debug) {
     # if we are recording inputs, just push the query string and fill list onto the
     # global sqlinput list
     push @sqlinput, "$querystring (".join(",",map {"'$_'"} @fill).")";
-  }
-  print "connecting with $user, $passwd<br>";
-  my $dbh = DBI->connect("DBI:Oracle:",$user,$passwd);
-  if (not $dbh) {
-    print "failed";
+}
+my $dbh = DBI->connect("DBI:Oracle:",$user,$passwd);
+if (not $dbh) {
+	print "failed";
     # if the connect failed, record the reason to the sqloutput list (if set)
     # and then die.
     if ($debug) {
-      push @sqloutput, "<b>ERROR: Can't connect to the database because of ".$DBI::errstr."</b>";
+    	push @sqloutput, "<b>ERROR: Can't connect to the database because of ".$DBI::errstr."</b>";
     }
     die "Can't connect to database because of ".$DBI::errstr;
-  }
-  print "prepare: $querystring";
-  my $sth = $dbh->prepare($querystring) or die $DBI::errstr;
-  if (not $sth) {
+}
+my $sth = $dbh->prepare($querystring) or die $DBI::errstr;
+if (not $sth) {
     #
     # If prepare failed, then record reason to sqloutput and then die
     #
-    print "failed";
     push @sqloutput, "<b>ERROR: Can't prepare '$querystring' because of ".$DBI::errstr."</b>";
 
     my $errstr="Can't prepare $querystring because of ".$DBI::errstr;
     $dbh->disconnect();
     die $errstr;
-  }
-  print "fill: @fill";
-  if (not $sth->execute(@fill)) {
+}
+if (not $sth->execute(@fill)) {
     # if (not $sth->execute) {
-    print "failed";
     #
     # if exec failed, record to sqlout and die.
 
@@ -132,43 +127,69 @@ sub ExecSQL {
     my $errstr="Can't execute $querystring with fill (".join(",",map {"'$_'"} @fill).") because of ".$DBI::errstr;
     $dbh->disconnect() or die $DBI::errstr;
     die $errstr;
-  }
+}
 #
 # The rest assumes that the data will be forthcoming.
 #
 #
-  my @data;
-  if (defined $type and $type eq "ROW") {
-    @data=$sth->fetchrow_array();
-    $sth->finish();
-    if ($debug) {push @sqloutput, MakeTable("debug_sqloutput","ROW",undef,@data);}
-    $dbh->disconnect();
-    return @data;
-  }
-  my @ret;
-  while (@data=$sth->fetchrow_array()) {
-    push @ret, [@data];
-  }
-  if (defined $type and $type eq "COL") {
-    @data = map {$_->[0]} @ret;
-    $sth->finish();
-    if ($debug) {push @sqloutput, MakeTable("debug_sqloutput","COL",undef,@data);}
-    $dbh->disconnect();
-    return @data;
-  }
-  $sth->finish();
-  push @sqloutput, MakeTable("debug_sql_output","2D",undef,@ret);
-  $dbh->disconnect();
-  return @ret;
+my @data;
+if (defined $type and $type eq "ROW") {
+	@data=$sth->fetchrow_array();
+	$sth->finish();
+	if ($debug) {push @sqloutput, MakeTable("debug_sqloutput","ROW",undef,@data);}
+	$dbh->disconnect();
+	return @data;
+}
+my @ret;
+while (@data=$sth->fetchrow_array()) {
+	push @ret, [@data];
+}
+if (defined $type and $type eq "COL") {
+	@data = map {$_->[0]} @ret;
+	$sth->finish();
+	if ($debug) {push @sqloutput, MakeTable("debug_sqloutput","COL",undef,@data);}
+	$dbh->disconnect();
+	return @data;
+}
+$sth->finish();
+push @sqloutput, MakeTable("debug_sql_output","2D",undef,@ret);
+$dbh->disconnect();
+return @ret;
 }
 
 
 
-print "Content-type: text/html\n\n";
+if ($ENV{'REQUEST_METHOD'} eq "POST") {
+	if(param("action") eq "login") {
+		my $user = param("user");
+		my $pass = param("password");
+		if (ValidUser($user,$pass)){
+
+				my $cookie=cookie(-name=>"login",
+					-value=>"$user", -expires =>"+10h");
+				push @outputcookies, $cookie;
+
+			print header(-expires=>'now', -cookie=>\@outputcookies);
+
+		}
+		else {
+			 print "Content-type: text/html\n\n";
+		}
+	}
+	else {
+		 print "Content-type: text/html\n\n";
+	}
+}
+else {
+	 print "Content-type: text/html\n\n";
+}
+
+
+
 
 print "<html>";
 print "<head>";
-print "<script src=\"\/\/ajax.googleapis.com\/ajax\/libs\/jquery\/1.8.2\/jquery.min.js\/\"></script>";
+print "<script src=\"//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js\"></script>";
 print "<link href=\"\/\/netdna.bootstrapcdn.com\/twitter-bootstrap\/2.1.1\/css/bootstrap-combined.min.css\" rel=\"stylesheet\">";
 print "<title>Portfolio</title>";
 print "</head>";
@@ -197,13 +218,13 @@ print "<div class=\"hero-unit\">";
 # find its butt
 #
 BEGIN {
-  unless ($ENV{BEGIN_BLOCK}) {
-    use Cwd;
-    $ENV{ORACLE_BASE}="/raid/oracle11g/app/oracle/product/11.2.0.1.0";
-    $ENV{ORACLE_HOME}=$ENV{ORACLE_BASE}."/db_1";
-    $ENV{ORACLE_SID}="CS339";
-    $ENV{LD_LIBRARY_PATH}=$ENV{ORACLE_HOME}."/lib";
-    $ENV{BEGIN_BLOCK} = 1;
-    exec 'env',cwd().'/'.$0,@ARGV;
-  }
+	unless ($ENV{BEGIN_BLOCK}) {
+		use Cwd;
+		$ENV{ORACLE_BASE}="/raid/oracle11g/app/oracle/product/11.2.0.1.0";
+		$ENV{ORACLE_HOME}=$ENV{ORACLE_BASE}."/db_1";
+		$ENV{ORACLE_SID}="CS339";
+		$ENV{LD_LIBRARY_PATH}=$ENV{ORACLE_HOME}."/lib";
+		$ENV{BEGIN_BLOCK} = 1;
+		exec 'env',cwd().'/'.$0,@ARGV;
+	}
 }
--- login.pl
+++ login.pl
@@ -1,33 +1,31 @@
 #!/usr/bin/perl -w
 require "header.pl";
-use Data::Dumper;
 
 sub ValidUser {
-  my ($user,$password)=@_;
-  my @col;
-  eval {@col=ExecSQL($dbuser,$dbpasswd, "select count(*) from stockuser where email='$user' and password='$password'",undef);};
+	my ($user,$password)=@_;
+	my @col;
+	eval {@col=ExecSQL($dbuser,$dbpasswd, "select count(*) from stockuser where email='$user' and password='$password'",undef);};
 
-  if ($@) {
-    return 0;
-  } else {
-    return $col[0]>0;
-  }
-}
-if ($ENV{'REQUEST_METHOD'} eq "POST") {
-  my $user = param("user");
-  my $pass = param("password");
-  my @rows;
-  print ValidUser($user,$pass);
-}
+	if ($@) {
+		return 0;
+		} else {
+			return $col[0]>0;
+		}
+	}
+	if ($ENV{'REQUEST_METHOD'} eq "POST") {
+		print "<h2>Thanks for logging in!</h2>";
+		print "<a href=\"home.pl\" class= \"btn\">Go Home!</a>";
+	}
 
-else {
+	else {
 
-  print start_form(-name=>'Login', -type=>"post"),
-  h2('Login!'), "<fieldset>",
-  "Name: ",textfield(-name=>'user'),"<br><br>",
-  "Password: ",password_field(-name=>'password'), "<br><br>";
-  print "<input type=\"submit\" class=\"btn btn-primary\">","</fieldset>";
+		print start_form(-name=>'Login', -type=>"post"),
+		h2('Login!'), "<fieldset>",
+		hidden(-name=>'action',default=>['login']),
+		"Name: ",textfield(-name=>'user'),"<br><br>",
+		"Password: ",password_field(-name=>'password'), "<br><br>";
+		print "<input type=\"submit\" class=\"btn btn-primary\">","</fieldset>";
 
 
-}
-require "footer.pl";
+	}
+	require "footer.pl";

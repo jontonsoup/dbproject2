545d55dd6faf7a5dd2c9c351daba9ed91cae6868
Jonathan Friedman <friedmanj98@gmail.com>
2012-11-12 12:24:47 -0600

    BROKEN: Portfolio stocks framework added

--- portfolio.pl
+++ portfolio.pl
@@ -13,13 +13,23 @@ sub CashHoldings {
   if ($@) {
     print "there was an error";
   } else {
-    # the query is right but the data is wrong. Probably because
-    # cash_holdings is not a string
     my $cash = $col[0];
     print "<h2>Cash holdings: $col[0]</h2>";
   }
 }
 
+sub get_stocks {
+   eval {@col=ExecSQL($dbuser, $dbpasswd,
+         "select symbol, amount from stocks, hasstock where email = '$login'",
+         "ROW" )};
+  if ($@) {
+    print "there was an error";
+  } else {
+  }
+
+}
+
 CashHoldings();
+get_stocks();
 
 require "footer.pl";

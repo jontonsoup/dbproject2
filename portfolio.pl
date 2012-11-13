7353310862e5f1b47dff578c9f4a0398e37408ce
Jonathan Friedman <friedmanj98@gmail.com>
2012-11-12 20:53:37 -0600

    Bug Fix: cah holdings show up on portfolio

--- portfolio.pl
+++ portfolio.pl
@@ -8,10 +8,10 @@ my $login = cookie('login');
 
 sub CashHoldings {
   eval {@col=ExecSQL($dbuser, $dbpasswd,
-		     "select cash_holdings from stockuser where email='$login'",
+		     "select cashholding from transaction where email='$login' AND rownum<=1 order by ts DESC ",
 		     "COL" )};
   if ($@) {
-    print "there was an error";
+    print "there was an error<br> $DBI::errstr<br>";
   } else {
     my $cash = $col[0];
     print "<h2>Cash holdings: $col[0]</h2>";
@@ -23,7 +23,7 @@ sub get_stocks {
          "select symbol, amount from stocks, hasstock where email = '$login'",
          "ROW" )};
   if ($@) {
-    print "there was an error";
+    print "there was an error <br>$DBI::errstr<br>";
   } else {
   }
 

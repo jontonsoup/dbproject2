2195b38e71e3b9ca74a9cc5828c3f9577b3840a1
Gursimran Singh <g13singh@gmail.com>
2012-11-13 23:37:41 -0600

    made history work, kinda. test this plz.

--- history.pl
+++ history.pl
@@ -15,8 +15,30 @@ print "
 
 # collects and tabulates history of transactions for current user
 sub HistoryTable {
-  
+  @ret = sql_jon_version("select ts, symbol, price, quantity, type, cashholding from transaction where email='$login' AND rownum<=10 order by ts DESC");
+  #print Dumper(@ret);
+  print "<table class=\"table table-striped\">";
+  print "<thead>";
+  print "<tr><td>Timestamp</td><td>Symbol</td><td>Price</td><td>Quantity</td><td>Type</td><td>Cash Holding</td></tr>";
+  print "<tbody>";
+  foreach $row (@$ret){
+    print "<tr>";
+    foreach $next (@$row){
+       print "<td>$next</td>";
+    }
+    #print "<td><a class=\"btn\" href=\"pastperformance.pl?stock=" . $ret->[0]->[0] . "\">Go</a></td>";
+    #print "<td><a class=\"btn\" href=\"futureperformance.pl?stock=" . $ret->[0]->[0] . "\">Go</a></td>";
+    #print "<td><a class=\"btn\" href=\"strategy.pl?stock=" . $ret->[0]->[0] . "\">Go</a></td>";
+    print "</tr>";
+  }
+
+
+  print "</tbody>";
+  print "</table>";
+
 }
+
+HistoryTable();
 
 require "footer.pl";
 

6e51048facc785593751c0dc67d543bf22c76fea
Jonathan Friedman <friedmanj98@gmail.com>
2012-11-14 15:43:26 -0600

    Feature: added title to past perf

--- pastperformance.pl
+++ pastperformance.pl
@@ -3,6 +3,8 @@ require "header.pl";
 
 $stock = param("stock");
 
+print "<h2>History for $stock</h2>";
+
 print "<img class=\"img\" src=\"http:\/\/murphy.wot.eecs.northwestern.edu\/~jmf716\/portfolio\/plot_stock.pl?type=plot&symbol=$stock\">";
 
 

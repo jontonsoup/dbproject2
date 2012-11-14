1ec079fcd54badbe1782ab0ae889bb206c0a0d64
Jonathan Friedman <friedmanj98@gmail.com>
2012-11-14 00:22:36 -0600

    Bug Fix: permissions

--- futureperformance.pl
+++ futureperformance.pl
@@ -1,8 +1,7 @@
 #!/usr/bin/perl -w
 require "header.pl";
-$symbol = "APPL";#param("stock");
+$symbol = param("stock");
 
 print `time_series_symbol_project.pl ORCL 8 AWAIT 300 ARIMA 2 1 2 | tail -20 2>&1`;
-print `env`;
 
 require "footer.pl";

cbd26acd3bd5a0c01e01ad9412e08eba409fb14f
Gursimran Singh <g13singh@gmail.com>
2012-11-17 18:04:21 -0600

    - compute coeff of var for given stock and time interval   - not auto computed for all stocks yet   - time interval can be specified

---
+++ get_info_cvb.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+
+use Getopt::Long;
+use Time::ParseDate;
+use FileHandle;
+
+use stock_data_access;
+
+$close=1;
+
+$field='close';
+
+&GetOptions("field=s" => \$field,
+	    "from=s" => \$from,
+	    "to=s" => \$to);
+
+if (defined $from) { $from=parsedate($from);}
+if (defined $to) { $to=parsedate($to); }
+
+
+$#ARGV>=0 or die "usage: get_info.pl [--field=field] [--from=time] [--to=time] SYMBOL+\n";
+
+#print join("\t","symbol","field","num","mean","std","min","max","cov"),"\n";
+
+while ($symbol=shift) {
+  #$sql = "select count($field), avg($field), stddev($field), min($field), max($field)  
+  #        from ".GetStockPrefix()."StocksDaily where symbol='$symbol'
+  #        ";
+  $sql = "select count($field), avg($field), stddev($field), min($field), max($field) 
+           from 
+           (select symbol, close, timestamp from ".GetStockPrefix()."StocksDaily
+           union all
+           select symbol, close, ts as timestamp from stocksdaily)
+           where symbol='$symbol'
+          ";
+  $sql.= " and timestamp>=$from" if $from;
+  $sql.= " and timestamp<=$to" if $to;
+
+
+  ($n,$mean,$std,$min,$max) = ExecStockSQL("ROW",$sql);
+
+  if ($n != 0) {
+    print join("\t",$symbol,$field, $n, $mean, $std, $min, $max, $std/$mean),"\n";
+  } else {
+    print "$n results\n";
+  }
+  
+}
--- stats.pl
+++ stats.pl
@@ -50,12 +50,54 @@ sub get_stocks {
 
 }
 
-get_stocks();
+#get_stocks();
 
 # get_info.pl can be used to get stddev, avg, mean, min, and max of a stock over from and to timestamp
 # we also need current stocks info so...custom get_info.pl?
 # coeff of var for a stock = stdev/mean
 
+
+#print join("\t", split(" ", `./get_info_cvb.pl AAPL --from "8 years ago" --to "7 years ago"`));
+
+
+if ($ENV{'REQUEST_METHOD'} eq "POST") {
+  print "<h3>Coefficient of Variance</h3>";
+  my $from = param('from');
+  my $to = param('to');
+  print join("\t", split(" ", `./get_info_cvb.pl AAPL --from "$from" --to "$to"`));
+  
+  $ret = sql_jon_version("select symbol, amount from stocks natural join hasstock where portfolio_id='$portfolio_id'");
+
+  print "<h2>Current Portfolio</h2>";
+  print "<table class=\"table table-striped\">";
+  print "<thead>";
+  print "<tr><td>Stock</td><td># Shares</td><td>Past Performance</td><td>Future Performance</td><td>Strategy</td></tr>";
+  print "<tbody>";
+  foreach $row (@$ret){
+    print "<tr>";
+    foreach $next (@$row){
+      print "<td>$next</td>";
+    }
+    print "<td><a class=\"btn\" href=\"pastperformance.pl?stock=" . $row->[0] . "\">Go</a></td>";
+    print "<td><a class=\"btn\" href=\"futureperformance.pl?stock=" . $row->[0] . "\">Go</a></td>";
+    print "<td><a class=\"btn\" href=\"strategy.pl?stock=" . $row->[0] . "\">Go</a></td>";
+    print "</tr>";
+  }
+
+
+  print "</tbody>";
+  print "</table>";
+
+} else {
+  
+  print start_form(-name=>'TimeFrame', -type=>"post"),
+  h2('TimeFrame'), "<fieldset>",
+  hidden(-name=>'portfolio_id',default=>["$portfolio_id"]),
+  "From: ",textfield(-name=>'from'),"<br><br>",
+  "To: ",textfield(-name=>'to'), "<br><br>";
+  print "<input type=\"submit\" class=\"btn btn-primary\">","</fieldset>";
+  
+}
 
 require "footer.pl";
 

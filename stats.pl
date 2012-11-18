c97763bb0b0995f270fbf10df7550e02d0237709
Gursimran Singh <g13singh@gmail.com>
2012-11-17 22:39:36 -0600

    - make covariance matrix depend on all stock data, historic and current - try out making a view to mesh historic and current stock data   - tables in different databases; more work than expected

--- stats.pl
+++ stats.pl
@@ -103,7 +103,7 @@ if ($ENV{'REQUEST_METHOD'} eq "POST") {
   print "<hr>";
 
   print "<h3>Covariance Matrix</h3>";
-  $out = `./get_covar.pl --from "$from" --to "$to" --corrcoeff @symbols`;
+  $out = `./get_covar_all.pl --from "$from" --to "$to" --corrcoeff @symbols`;
   print replace("\t", "&nbsp;&nbsp;&nbsp;&nbsp;", replace("\n", "<br>", $out));
 
 } else {
